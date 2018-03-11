9b1dd21eeeb1a2edf011cc219967a20b6ed587f1
Daniel Schweighöfer <SchweighoeferD@gmail.com>
2018-03-12 00:39:42 +0100

    Update icon links

--- src/app/icon/icon.component.ts
+++ src/app/icon/icon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 import { LuJsonService } from '../lu-json.service';
 import { Icons } from '../cdclient';
@@ -12,8 +13,17 @@ export class IconComponent implements OnInit {
 
   @Input() iconID: number;
   icon: Icons;
+  baseUrl: string;
 
-  constructor(private luJsonService: LuJsonService) { }
+  constructor(private luJsonService: LuJsonService) {
+    if (environment.production) {
+      // using public API
+      this.baseUrl = 'https://xiphoseer.github.io/';
+    } else {
+      // serving API locally
+      this.baseUrl = 'http://localhost:8000/';
+    }
+  }
 
   ngOnInit() {
   	this.getIcon();
