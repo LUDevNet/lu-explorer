341dda877b81597a4ac470bc0575f05e6becb141
Daniel Seiler <daniel.g.seiler+lugh@gmail.com>
2019-04-05 16:34:48 +0200

    Cleanup + NexusHQ

--- src/app/objects/components/components.ts
+++ src/app/objects/components/components.ts
@@ -2,6 +2,7 @@ import { ItemComponentComponent } from './item-component/item-component.componen
 import { PackageComponentComponent } from './package-component/package-component.component';
 import { QuickBuildComponentComponent } from './quick-build-component/quick-build-component.component';
 import { RebuildComponentComponent } from './rebuild-component/rebuild-component.component';
+import { RenderComponentComponent } from './render-component/render-component.component';
 import { ScriptComponentComponent } from './script-component/script-component.component';
 import { ScriptedActivityComponentComponent } from './scripted-activity-component/scripted-activity-component.component';
 import { ShootingGalleryComponentComponent } from './shooting-gallery-component/shooting-gallery-component.component';
@@ -12,6 +13,7 @@ export {
   PackageComponentComponent,
   QuickBuildComponentComponent,
   RebuildComponentComponent,
+  RenderComponentComponent,
   ScriptComponentComponent,
   ScriptedActivityComponentComponent,
   ShootingGalleryComponentComponent,
