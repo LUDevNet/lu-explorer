785a345428fc231eaed9f97737d1991d2eb38a3b
Daniel Seiler <daniel.g.seiler+lugh@gmail.com>
2021-01-04 03:47:38 +0100

    Add more components to obj-instance

--- src/app/zones/obj-instance/obj-instance.component.ts
+++ src/app/zones/obj-instance/obj-instance.component.ts
@@ -27,6 +27,7 @@ const RENDER_KEYS = [
   'renderCullingGroup',
   'ignoreCameraCollision',
   'camPrefersToFadeObject',
+  'setsRailCam',
   'camGradSnap',
   'CamRlBiasAmt',
   'CamRlBiasBi',
@@ -51,6 +52,7 @@ interface RenderSettings {
   ignoreCameraCollision?: any,
   camPrefersToFadeObject?: any,
   camGradSnap?: any,
+  setsRailCam?: boolean,
   CamRlBiasAmt?: any,
   CamRlBiasBi?: any,
   CamRlBiasFwd?: any,
@@ -78,12 +80,29 @@ const SIMPLE_PHYSICS_KEYS = [
   'respawnVol',
   'respawnVolName',
 ];
-const SCRIPT_KEYS = [
+interface SimplePhysicsSettings {
+  create_physics?: any,
+  friction?: any,
+  navmesh_carver?: any,
+  carver_only?: any,
+  add_to_navmesh?: any,
+  is_smashable?: any,
+  respawn?: any,
+  respawnVol?: any,
+  respawnVolName?: any,
+}
+const SCRIPT_KEYS = [ // 5
   'custom_script_client',
   'custom_script_server',
 ];
+const DESTRUCTIBLE_KEYS = [ // 7
 
-const SPAWNER_KEYS = [
+];
+interface DestructibleSettings {
+
+}
+
+const SPAWNER_KEYS = [ // 10
   'spawntemplate',
   'spawner_active_on_load',
   'active_on_load',
@@ -109,19 +128,59 @@ interface SpawnerSettings {
   spawnNetNameForSpawnGroupOnSmash?: string,
   groupID?: any,
 }
-const ITEM_KEYS = [];
-const SCRIPTED_ACTIVITY_KEYS = [
+const ITEM_KEYS = [ // 11
+];
+const COLLECTIBLE_KEYS = [ // 23
+
+];
+interface CollectibleSettings {
+
+}
+const SCRIPTED_ACTIVITY_KEYS = [ // 39
   'actIDovrd',
   'activityID',
 ];
-const PHANTOM_PHYSICS_KEYS = [
+const PROPERTY_KEYS = [ // 36
+  "propertyBordersLOT",
+  "propertyName",
+  "propertyVendorLOT",
+];
+interface PropertySettings {
+  propertyBordersLOT?: number,
+  propertyName?: string,
+  propertyVendorLOT?: number,
+}
+const PHANTOM_PHYSICS_KEYS = [ // 40
   'create_physics',
   'add_to_navmesh',
   'carver_only',
   'navmesh_carver',
   'is_smashable',
+  'cancelBehaviorMovement',
+  'respawn',
+  'respawnVol',
+  'respawnVolName',
+  'rlLeadIn',
+  'rlLeadOut',
+  'rspPos',
+  'rspRot',
 ];
-const PROPERTY_ENTRANCE_KEYS = [
+interface PhantomPhysicsSettings {
+  create_physics: boolean,
+  add_to_navmesh: boolean,
+  carver_only: boolean,
+  navmesh_carver: boolean,
+  is_smashable: boolean,
+  cancelBehaviorMovement: boolean,
+  respawn: number,
+  respawnVol: boolean,
+  respawnVolName: any,
+  rlLeadIn: any,
+  rlLeadOut: any,
+  rspPos: any,
+  rspRot: any,
+}
+const PROPERTY_ENTRANCE_KEYS = [ // 43
   "propertyDefaultTargetZone",
   "propertyLaunchNavGroupID",
   "propertyLaunchPadOnPropertyMap",
@@ -133,8 +192,29 @@ interface PropertyEntranceSettings {
   propertyLaunchNavGroupID: string,
   propertyLaunchPadOnPropertyMap: boolean,
   propertyName: string,
+  mapID: number,
 }
-const HF_LIGHT_DIRECTION_GADGET_KEYS = [
+const PROPERTY_MANAGEMENT_KEYS = [ // 45
+
+];
+interface PropertyManagementSettings {
+
+}
+const SOUND_AMBIENT_3D_KEYS = [ // 56
+  'NDAEG',
+  'NDAudioEventGUID',
+];
+interface SoundAmbient3DSettings {
+  NDAEG: string,
+  NDAudioEventGUID: string,
+}
+const PROPERTY_VENDOR_KEYS = [ // 65
+  "property_required_mission"
+]
+interface PropertyVendorSettings {
+  property_required_mission?: number,
+}
+const HF_LIGHT_DIRECTION_GADGET_KEYS = [ // 66
 
 ];
 const ROCKET_LAUNCH_KEYS = [
@@ -153,21 +233,51 @@ const ROCKET_LAUNCH_KEYS = [
   'rocketUseAltLandingPrecondition',
   'rocketUseLaunchPrecondition',
 ];
+interface RocketLaunchSettings {
+  rocketLaunchMusic?: string,
+  rocketLaunchPreCondition?: string,
+  rocketLaunchUseDBDefaults?: boolean,
+  summaryCamera?: string,
+  playSummary?: boolean,
+  launchCamera?: string,
+  targetScene?: string,
+  targetZone?: number,
+  playerAnim?: string,
+  rocketAltLandingPrecondition?: string,
+  rocketAltLandingSpawnName?: string,
+  rocketAnim?: string,
+  rocketUseAltLandingPrecondition?: boolean,
+  rocketUseLaunchPrecondition?: boolean,
+}
 const PROXMIMITY_MONITOR_KEYS = [];
+
+const PROPERTY_PLAQUE_KEYS = [ // 113
+
+];
+interface PropertyPlaqueSettings {
+
+}
 
 const KEYS = {
   "-1": GENERAL_KEYS,
   "2": RENDER_KEYS,
   "3": SIMPLE_PHYSICS_KEYS,
   "5": SCRIPT_KEYS,
+  "7": DESTRUCTIBLE_KEYS,
   "10": SPAWNER_KEYS,
   "11": ITEM_KEYS,
+  "23": COLLECTIBLE_KEYS,
+  "36": PROPERTY_KEYS,
   "39": SCRIPTED_ACTIVITY_KEYS,
   "40": PHANTOM_PHYSICS_KEYS,
   "43": PROPERTY_ENTRANCE_KEYS,
+  "45": PROPERTY_MANAGEMENT_KEYS,
+  "56": SOUND_AMBIENT_3D_KEYS,
+  "65": PROPERTY_VENDOR_KEYS,
   "66": HF_LIGHT_DIRECTION_GADGET_KEYS,
   "67": ROCKET_LAUNCH_KEYS,
   "78": PROXMIMITY_MONITOR_KEYS,
+  "113": PROPERTY_PLAQUE_KEYS,
 };
 
 @Component({
@@ -188,17 +298,23 @@ export class ObjInstanceComponent implements OnInit {
   remainingSettings: object;
 
   renderSettings?: RenderSettings;
-  simplePhysicsSettings?: object;
-  spawnerSettings?: SpawnerSettings;
-  scriptSettings?: object;
-  componentSettings?: object;
-  itemSettings?: object;
-  scriptedActivitySettings?: object;
-  phantomPhysicsSettings?: object;
-  propertyEntranceSettings?: object;
-  hfLightDirectionGadgetSettings?: object;
-  rocketLaunchSettings?: object;
+  simplePhysicsSettings?: SimplePhysicsSettings;
+  scriptSettings?: object; // 5
+  destructibleSettings?: DestructibleSettings; // 7
+  spawnerSettings?: SpawnerSettings; // 10
+  itemSettings?: object; // 11
+  collectibleSettings?: CollectibleSettings; // 23
+  propertySettings?: PropertySettings; // 36
+  scriptedActivitySettings?: object; // 39
+  phantomPhysicsSettings?: PhantomPhysicsSettings; // 40
+  propertyEntranceSettings?: PropertyEntranceSettings; // 43
+  propertyManagementSettings?: PropertyManagementSettings; // 45
+  soundAmbient3DSettings?: SoundAmbient3DSettings; // 56
+  propertyVendorSettings?: PropertyVendorSettings; // 65
+  hfLightDirectionGadgetSettings?: object; // 66
+  rocketLaunchSettings?: RocketLaunchSettings; // 67
   proximityMonitorSettings?: object;
+  propertyPlaqueSettings?: PropertyPlaqueSettings; // 113
 
   constructor(private luJsonService: LuJsonService) { }
 
@@ -216,14 +332,21 @@ export class ObjInstanceComponent implements OnInit {
       "2": x => this.renderSettings = x,
       "3": x => this.simplePhysicsSettings = x, // TODO: phantomOnly, markedAsPhantom
       "5": x => this.scriptSettings = x,
+      "7": x => this.destructibleSettings = x,
       "10": x => this.spawnerSettings = x,
       "11": x => this.itemSettings = x,
+      "23": x => this.collectibleSettings = x,
+      "36": x => this.propertySettings = x,
       "39": x => this.scriptedActivitySettings = x,
       "40": x => this.phantomPhysicsSettings = x,
       "43": x => this.propertyEntranceSettings = x,
+      "45": x => this.propertyManagementSettings = x,
+      "56": x => this.soundAmbient3DSettings = x,
+      "65": x => this.propertyVendorSettings = x,
       "66": x => this.hfLightDirectionGadgetSettings = x,
       "67": x => this.rocketLaunchSettings = x,
       "78": x => this.proximityMonitorSettings = x,
+      "113": x => this.propertyPlaqueSettings = x,
     };
     Object.values(setters).forEach(x => x(undefined));
 
