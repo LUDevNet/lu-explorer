ec05a317ce42036d32da0b35e104ce780d826f90
Daniel Seiler <daniel.g.seiler+lugh@gmail.com>
2021-01-09 01:41:37 +0100

    Add preliminary zone gifs and remove json from inventory component

--- src/app/zones/zone-detail/zone-detail.component.ts
+++ src/app/zones/zone-detail/zone-detail.component.ts
@@ -15,6 +15,7 @@ export class ZoneDetailComponent implements OnInit {
 
   @Input() zone: ZoneDetail;
   locale: any;
+  zone_id: number;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,10 +24,18 @@ export class ZoneDetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.zone_id = -1;
     this.route.paramMap.subscribe(map => this.getZone(+map.get('id')));
   }
 
+  zoneGif() {
+    if ([1001, 1100, 1149, 1150, 1151, 1200, 1201, 1250, 1251, 1260, 1300, 1350, 1351, 1400, 1450, 1451, 1600, 1800, 1900, 2000].includes(this.zone_id)) {
+      return `textures/ui/signage/${this.zone_id}.gif`;
+    }
+  }
+
   getZone(id: number): void {
+    this.zone_id = id;
     this.luJsonService.getZone(id).subscribe(zone => this.zone = zone);
   }
 }
