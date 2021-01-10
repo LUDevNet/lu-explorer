782b0cab0b1dbf0ce12f62c034e8601bdf7feb97
Daniel Seiler <daniel.g.seiler+lugh@gmail.com>
2021-01-10 23:08:24 +0100

    Add image to activities; add XML to lu-res

--- src/app/util/services/lu-res.service.ts
+++ src/app/util/services/lu-res.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -8,7 +11,7 @@ export class LuResService {
 
   private baseUrl: string = "";
 
-  constructor() {
+  constructor(private http: HttpClient) {
     if (environment.production) {
       // using public API
       this.baseUrl = 'https://xiphoseer.github.io/';
@@ -24,5 +27,13 @@ export class LuResService {
 
   getResolvedResUrl(path: string): string {
     return this.baseUrl + 'lu-res/' + path;
+  }
+
+  getXML(path: string): Observable<XMLDocument> {
+    const parser = new DOMParser();
+    return this.http.get(this.getResolvedResUrl(path + '.xml'), { responseType: 'text'})
+    .pipe(map(xmlStr => {
+      return parser.parseFromString(xmlStr, "application/xml");
+    }));
   }
 }
