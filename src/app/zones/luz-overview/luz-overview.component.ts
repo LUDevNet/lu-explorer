d4c2348fe019817fa3fc47a68dbb677238640dda
lcdr <lcdr@users.noreply.github.com>
2021-07-26 19:43:33 +0200

    wip

---
+++ src/app/zones/luz-overview/luz-overview.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-luz-overview",
+  templateUrl: "./luz-overview.component.html",
+  styleUrls: ["./luz-overview.component.css"]
+})
+export class LuzOverviewComponent {
+}
--- src/app/zones/zones-routing.module.ts
+++ src/app/zones/zones-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LuzOverviewComponent } from './luz-overview/luz-overview.component';
+import { LuzPathComponent } from './luz-path/luz-path.component';
+import { LuzPathsComponent } from './luz-paths/luz-paths.component';
+import { LuzPathWaypointComponent } from './luz-path-waypoint/luz-path-waypoint.component';
 import { ZoneIndexComponent } from './zone-index/zone-index.component';
 import { ZoneDetailComponent } from './zone-detail/zone-detail.component';
 import { ScenesComponent } from './scenes/scenes.component';
 
 const zonesRoutes: Routes = [
-  { path: ':id', component: ZoneDetailComponent, data: { title: params => `Zone #${params['id']}` } },
-  { path: ':id/:sc', component: ScenesComponent, data: { title: params => `Scene #${params['sc']} | Zone #${params['id']}` } },
-  { path: ':id/:sc/:obj', component: ScenesComponent, data: { title: params => `Object #${params['obj']} | Scene #${params['sc']} | Zone #${params['id']}` } },
-  { path: '', component: ZoneIndexComponent, data: { title: "Zones" } }
+  { path: '', component: ZoneIndexComponent, data: { title: "Zones" } },
+  {
+    path: ':id', component: ZoneDetailComponent, data: { title: params => `Zone #${params['id']}` },
+    children: [
+      { path: '', component: LuzOverviewComponent },
+      { path: 'paths', component: LuzPathsComponent, data: { title: params => `Paths | Zone #${params['id']}` } },
+      { path: 'paths/:path', component: LuzPathComponent, data: { title: params => `Path ${params['path']} | Zone #${params['id']}` } },
+      { path: 'paths/:path/:waypoint', component: LuzPathWaypointComponent, data: { title: params => `Waypoint #${params['waypoint']} | Path ${params['path']} | Zone #${params['id']}` } }
+    ]
+  },
+  { path: ':id/scenes/:sc', component: ScenesComponent, data: { title: params => `Scene #${params['sc']} | Zone #${params['id']}` } },
+  { path: ':id/scenes/:sc/:obj', component: ScenesComponent, data: { title: params => `Object #${params['obj']} | Scene #${params['sc']} | Zone #${params['id']}` } }
 ]
 
 @NgModule({
