e8de0a657e625626e577522e09dd23988e701351
Daniel Seiler <daniel.g.seiler+lugh@gmail.com>
2021-09-05 12:57:59 +0200

    Add test CI environment

---
+++ src/environments/environment.ci.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  data: {
+    apiUrl: "https://lu.lcdruniverse.org/explorer/api/",
+    jsonUrl: "https://xiphoseer.de/lu-json/",
+    resUrl: "https://xiphoseer.de/lu-res/"
+  },
+  firebase: {
+    apiKey: "AIzaSyCxUJm4DFGfimoeGKPGT7-9f7gD0frJZ64",
+    authDomain: "lu-explorer.firebaseapp.com",
+    databaseURL: "https://lu-explorer.firebaseio.com",
+    projectId: "lu-explorer",
+    storageBucket: "lu-explorer.appspot.com",
+    messagingSenderId: "863659386802",
+    appId: "1:863659386802:web:d6b83d775b9fd3ec3f66ab",
+    measurementId: "G-G44HHQ828G"
+  }
+};
