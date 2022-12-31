54d9f3e01efe4654e362bb5240f851cb775eda2e
Daniel Seiler <xiphoseer@mailbox.org>
2022-12-31 08:56:08 +0100

    gate-versions: load zone name

--- src/app/gates/detail/detail.component.ts
+++ src/app/gates/detail/detail.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay, switchMap } from 'rxjs/operators';
 import { Rev_GateVersion } from '../../../defs/api';
-import { Locale_WhatsCoolNewsAndTips, Locale_ZoneLoadingTips } from '../../../defs/locale';
+import { Locale_WhatsCoolNewsAndTips, Locale_ZoneLoadingTips, Locale_ZoneTable } from '../../../defs/locale';
 import { mapArr, pick } from '../../../defs/rx';
 import { LuCoreDataService } from '../../services';
 
@@ -20,6 +20,11 @@ interface WhatsCoolNews {
   $loc: Observable<Locale_WhatsCoolNewsAndTips>;
 }
 
+interface Zone {
+  id: number;
+  $loc: Observable<Locale_ZoneTable>;
+}
+
 @Component({
   selector: 'lux-gate-detail',
   templateUrl: './detail.component.html',
@@ -31,6 +36,7 @@ export class GateDetailComponent implements OnInit {
 
   $zoneLoadingTips: Observable<ZoneLoadingTip[]>;
   $whatsCoolNews: Observable<WhatsCoolNews[]>;
+  $zones: Observable<Zone[]>;
 
   constructor(private route: ActivatedRoute, private luCoreData: LuCoreDataService) { }
 
@@ -39,6 +45,7 @@ export class GateDetailComponent implements OnInit {
     this.$id = this.route.paramMap.pipe(map(p => p.get('id')));
     this.$data = this.$id.pipe(switchMap(id => this.luCoreData.getRevEntry<Rev_GateVersion>('gate-versions', id)));
 
+    // Zone Loading Tips
     let $zoneLoadingTipIds = this.$data.pipe(map(x => x.zone_loading_tips));
     let $zoneLoadingTipsLocale = $zoneLoadingTipIds.pipe(
       cd.queryLocaleNum$("ZoneLoadingTips", ["title", "tip1", "tip2"]),
@@ -48,6 +55,7 @@ export class GateDetailComponent implements OnInit {
       mapArr(id => new ZoneLoadingTip(id, $zoneLoadingTipsLocale.pipe(pick(id))))
     )
 
+    // What's Cool – News & Tips
     let $whatsCoolNewsIds = this.$data.pipe(map(data => data.whats_cool_news_and_tips));
     let $whatsCoolNewsLoc = $whatsCoolNewsIds.pipe(
       cd.queryLocaleNum$<Locale_WhatsCoolNewsAndTips>("WhatsCoolNewsAndTips", ["storyTitle", "text"]),
@@ -57,8 +65,22 @@ export class GateDetailComponent implements OnInit {
         return {
           id,
           $loc: $whatsCoolNewsLoc.pipe(pick(id)),
-        }
+        };
       })
     )
+
+    // Zones
+    let $zoneIds = this.$data.pipe(map(data => data.zones));
+    let $zonesLoc = $zoneIds.pipe(
+      cd.queryLocaleNum$<Locale_ZoneTable>("ZoneTable", ["DisplayDescription"]),
+    )
+    this.$zones = $zoneIds.pipe(
+      mapArr(id => {
+        return {
+          id,
+          $loc: $zonesLoc.pipe(pick(id))
+        };
+      })
+    );
   }
 }
