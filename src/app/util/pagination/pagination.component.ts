eaa90f081fa3d4d175efdf2798c2648fe3d46d31
Daniel Seiler <xiphoseer@mailbox.org>
2023-01-04 20:40:42 +0100

    fix: pagination links

--- src/app/util/pagination/pagination.component.ts
+++ src/app/util/pagination/pagination.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent {
 
   @Input() prefix: string;
   @Input() current: number;
@@ -13,7 +13,8 @@ export class PaginationComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  pageLink(page: number): string {
+    return `${this.prefix}/page/${page}`;
   }
 
 }
