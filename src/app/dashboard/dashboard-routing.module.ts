999bdc43d56434541d266395555a520e333d9114
Daniel Seiler <xiphoseer@mailbox.org>
2023-01-06 20:53:47 +0100

    Initial support for switching the backend

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,16 +1,18 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreditsComponent } from './credits/credits.component';
 
 import { DashboardComponent } from './dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const dashboardRoutes: Routes = [
   { path: '', component: DashboardComponent, data: { title: "Dashboard" } },
-  { path: 'credits', component: CreditsComponent, data: { title: "Credits" } }
+  { path: 'credits', component: CreditsComponent, data: { title: "Credits" } },
+  { path: 'settings', component: SettingsComponent, data: { title: "Settings" } },
 ]
 
 @NgModule({
-  imports: [ RouterModule.forChild(dashboardRoutes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forChild(dashboardRoutes)],
+  exports: [RouterModule]
 })
 export class DashboardRoutingModule { }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -7,16 +7,20 @@ import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { CreditsComponent } from './credits/credits.component';
 import { ChangelogComponent } from './changelog/changelog.component';
+import { SettingsComponent } from './settings/settings.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     CreditsComponent,
     ChangelogComponent,
+    SettingsComponent,
   ],
   imports: [
     CommonModule,
     UtilModule,
+    FormsModule,
     DashboardRoutingModule
   ]
 })
